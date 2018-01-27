6a07f8bb3bc9b60dbc720989d84f0e06f9aac642
alintulu <adelinalitu@gmail.com>
2018-01-27 23:10:55 +0200

    all my assets

--- Assets/Scripts/WallsScript.cs
+++ Assets/Scripts/WallsScript.cs
@@ -11,9 +11,9 @@ public class WallsScript : MonoBehaviour {
     // Use this for initialization
 
     void Start () {
-        colorMap.Add(new Color32(255, 255, 255, 255),0);     //white
-        colorMap.Add(new Color32(0, 0, 0, 255),1);          //black
-        colorMap.Add(new Color32(255, 20, 148, 255),2);     //pink
+        //colorMap.Add(new Color32(255, 255, 255, 255),0);     //white
+        colorMap.Add(new Color32(0, 0, 0, 255),0);          //tapet+panel
+        colorMap.Add(new Color32(0, 0, 255, 255),1);     //kakel
 
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         Texture2D tex = sr.sprite.texture;
@@ -44,9 +44,9 @@ public class WallsScript : MonoBehaviour {
                     tmpGO.transform.parent = objectGO[colorInteger].transform;
                         
                 } else {
-                    tmpGO = Instantiate(preFabs[0], new Vector3((float)(x*0.2), 0, (float)(y*0.2)), Quaternion.identity);
+                    //tmpGO = Instantiate(preFabs[0], new Vector3((float)(x*0.2), 0, (float)(y*0.2)), Quaternion.identity);
                     //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
-                    tmpGO.transform.parent = objectGO[0].transform;
+                    //tmpGO.transform.parent = objectGO[0].transform;
                 }
 
                
