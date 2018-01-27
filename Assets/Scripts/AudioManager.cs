71009577b18a5fff29c182958b6558cd61691885
Paul Vuorela <vuorpa13@gmail.com>
2018-01-28 00:44:26 +0200

    Game theme plus intensifying by traps

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,7 +9,7 @@ public class AudioManager : MonoBehaviour
 
 	public Sound[] sounds;
 
-	void Awake()
+	public void Awake()
 	{
 		if (instance != null)
 		{
@@ -32,13 +32,19 @@ public class AudioManager : MonoBehaviour
 		}
 	}
 
-	void Start() {
-		PlayMainMenuThemes();
+	public void Start() {
+		StartGameMusic();
 	}
 
-	void PlayMainMenuThemes() {
-		Sound menuIn = Array.Find(sounds, item => item.name == "MenuIn");
-		Sound menuLoop = Array.Find(sounds, item => item.name == "MenuLoop");
+	public void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			IntensifyGameThemeByTrap ();
+		}
+	}
+
+	public void PlayMainMenuThemes() {
+		Sound menuIn = GetSound("MenuIn");
+		Sound menuLoop = GetSound("MenuLoop");
 		if (menuIn == null || menuLoop == null)
 		{
 			Debug.LogWarning("Sounds for main menu theme not found!");
@@ -49,16 +55,78 @@ public class AudioManager : MonoBehaviour
 		menuLoop.source.PlayScheduled (AudioSettings.dspTime + menuIn.clip.length);
 	}
 
-	public void Play(string sound)
+	public void StartGameMusic() {
+		Stop ("MenuIn");
+		Stop ("MenuLoop");
+
+		Sound low = GetSound("LowIntensity");
+		if (low == null)
+		{
+			Debug.LogWarning("Low intensity game theme not found!");
+			return;
+		}
+
+		low.source.Play();
+	}
+
+
+	public void IntensifyGameThemeByTrap() {
+		Sound low = GetSound ("LowIntensity");
+		Sound medium = GetSound ("MediumIntensity");
+		Sound high = GetSound ("HighIntensity");
+		float clipLength = low.clip.length;
+
+		if (low == null || medium == null || high == null)
+		{
+			Debug.LogWarning("Sounds for game theme not found!");
+			return;
+		}
+
+		if (medium.source.isPlaying) {
+			float remainder = 3.75f - low.source.time % 3.75f;
+			high.source.time = medium.source.time + remainder;
+			high.source.PlayScheduled(AudioSettings.dspTime + remainder);
+			medium.source.SetScheduledEndTime(AudioSettings.dspTime + remainder);
+		}
+
+		if (low.source.isPlaying) {
+			float remainder = 3.75f - low.source.time % 3.75f;
+			Debug.LogWarning ("Remainder:" + remainder);
+			medium.source.time = low.source.time + remainder;
+			medium.source.PlayScheduled(AudioSettings.dspTime + remainder);
+			low.source.SetScheduledEndTime(AudioSettings.dspTime + remainder);
+		}
+	}
+
+	private void Play(string sound)
 	{
-		Sound s = Array.Find(sounds, item => item.name == sound);
+		Sound s = GetSound (name);
 		if (s == null)
 		{
-			Debug.LogWarning("Sound: " + name + " not found!");
 			return;
 		}
 
 		s.source.Play();
+	}
+
+	private void Stop(string sound)
+	{
+		Sound s = GetSound (name);
+		if (s == null)
+		{
+			return;
+		}
+		s.source.Stop();
+	}
+
+	private Sound GetSound(string name) {
+		Sound s = Array.Find (sounds, item => item.name == name);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return null;
+		}
+		return s;
 	}
 
 }
