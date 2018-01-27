92a22df9017b03b8c2f5f400535256bc3249a7bb
alintulu <adelinalitu@gmail.com>
2018-01-27 22:58:45 +0200

    Level scripts

---
+++ Assets/Scripts/WallsScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsScript : MonoBehaviour {
+
+	public Dictionary<Color32, int> colorMap = new Dictionary<Color32, int>();
+	public GameObject[] preFabs = new GameObject[10];
+	public GameObject[] objectGO = new GameObject[10];
+
+    // Use this for initialization
+
+    void Start () {
+        colorMap.Add(new Color32(255, 255, 255, 255),0);     //white
+        colorMap.Add(new Color32(0, 0, 0, 255),1);          //black
+        colorMap.Add(new Color32(255, 20, 148, 255),2);     //pink
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Texture2D tex = sr.sprite.texture;
+
+        int w = tex.width;
+        int h = tex.height;
+
+        Debug.Log("level loader");
+
+        Color32[] c = tex.GetPixels32();
+
+        for (int x = 0; x<w; x++)
+        {
+            for( int y = 0; y<h; y++)
+            {
+                //int colorInteger = colorMap[c[x+w*y]];
+				GameObject tmpGO;
+
+                //Debug.Log("HEJ");
+                //Debug.Log("COLOR: "+ c[x+w*y]);
+
+                if(colorMap.ContainsKey(c[x+w*y])) {
+
+                    int colorInteger = colorMap[c[x+w*y]];
+
+                    tmpGO = Instantiate(preFabs[colorInteger], new Vector3((float)(x*0.2), 0, (float)(y*0.2)), Quaternion.identity);
+                    //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
+                    tmpGO.transform.parent = objectGO[colorInteger].transform;
+                        
+                } else {
+                    tmpGO = Instantiate(preFabs[0], new Vector3((float)(x*0.2), 0, (float)(y*0.2)), Quaternion.identity);
+                    //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
+                    tmpGO.transform.parent = objectGO[0].transform;
+                }
+
+               
+					 
+			}
+        }
+            
+        sr.enabled = false;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
