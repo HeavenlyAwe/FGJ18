bdc71e430b490359ccded1dfa169d91f63498d92
Adelina Lintuluoto <Adelinalintu@gmail.com>
2018-05-05 20:33:41 +0300

    posters

--- Assets/Scripts/Level/PropsScript.cs
+++ Assets/Scripts/Level/PropsScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,43 +26,44 @@ public class PropsScript : MonoBehaviour {
 
         for (int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
+
                 GameObject tmpGO;
 
 
                 int index = (int)(c[x + w * y].r / 16);
 
+                if (x == 74 && y == 4) {
+                  Debug.Log("FIRST    index = " + index + " x: " +  x + " y :" + y);
+                }
+
 
                 if (index == 0) {
                     tmpGO = Instantiate(prefabs[0], new Vector3((float)(x * 0.2), 0, (float)(y * 0.2)), Quaternion.identity);
-                    //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
                     tmpGO.transform.parent = objectGO.transform;
                     Debug.Log("vial");
-                // } else if (index == 1) {
-                //     GameObject door = new GameObject();
-                //     door.transform.position = new Vector3(x * 0.2f, 0, y * 0.2f);
-                //     door.transform.parent = objectGO.transform;
-                //     door.transform.name = "door";
-                //     //tmpGO = Instantiate(preFabs[1], new Vector3((float)(x*0.2), 0, (float)(y*0.2)), Quaternion.identity);
-                //     //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
-                //     //tmpGO.transform.parent = objectGO[1].transform;
                 } else if (index < 6) {
                     Debug.Log(index);
                     tmpGO = Instantiate(prefabs[index], new Vector3((float)(x * 0.2), 0, (float)(y * 0.2)), Quaternion.identity);
-                    //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
                     tmpGO.transform.parent = objectGO.transform;
-
-
                 } else if (index < 7) {
-                    if (c[(x+1) + w * y].r == 0) {
-                        Debug.Log("rotate poster");
-                    tmpGO = Instantiate(prefabs[index], new Vector3((float)(x * 0.2), 0, (float)(y * 0.2)), Quaternion.Euler(0,180,0));
-                    //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
-                    tmpGO.transform.parent = objectGO.transform;
-                    } else {
-                    tmpGO = Instantiate(prefabs[index], new Vector3((float)(x * 0.2), 0, (float)(y * 0.2)), Quaternion.identity);
-                    //tmpGO = Instantiate(preFabs[colorInteger], new Vector3(x, 0, y), Quaternion.identity);
-                    tmpGO.transform.parent = objectGO.transform;
-                    }
+                  Debug.Log("index = " + index + " x: " +  x + " y :" + y);
+                  int left = x - 1;
+                  int index_l = (int)(c[left + w * y].r / 16);
+                  int right = x + 1;
+                  int index_r = (int)(c[right + w * y].r / 16);
+                  int up = y + 1;
+                  int index_u = (int)(c[x + w * up].r / 16);
+                  int down = y - 1;
+                  int index_d = (int)(c[x + w * down].r / 16);
+
+                  Debug.Log("index left = " + index_l + " x: " +  left + " y :" + y);
+                  Debug.Log("index right = " + index_r + " x: " +  right + " y :" + y);
+                  Debug.Log("index up = " + index_u + " x: " +  x + " y :" + up);
+                  Debug.Log("index down = " + index_d + " x: " +  x + " y :" + down);
+
+                  tmpGO = Instantiate(prefabs[index], new Vector3((float)(x * 0.2), 0, (float)(y * 0.2)), Quaternion.identity);
+                  tmpGO.transform.parent = objectGO.transform;
+
                 }
             }
         }
